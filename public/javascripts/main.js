f4e75c52cab95300ba7074dbc89c6d32b3191dfa
Boo Irizarry <booirizarryy@L02028138.niaid.nih.gov>
2017-12-10 13:31:01 -0500

    update about and login page and footer and header

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -14,3 +14,33 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+// rotate card in about page.
+function rotateCard(btn){
+        var $card = $(btn).closest('.card-container');
+        // console.log($card);
+        if($card.hasClass('hover')){
+            $card.removeClass('hover');
+        } else {
+            $card.addClass('hover');
+        }
+    }
+
+$(function() {
+
+    $('#login-form-link').click(function(e) {
+    	$("#login-form").delay(100).fadeIn(100);
+ 		$("#register-form").fadeOut(100);
+		$('#register-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+	$('#register-form-link').click(function(e) {
+		$("#register-form").delay(100).fadeIn(100);
+ 		$("#login-form").fadeOut(100);
+		$('#login-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+
+});
--- routes/index.js
+++ routes/index.js
@@ -2,29 +2,31 @@ var express = require('express');
 var router = express.Router();
 
 
-/* GET Index page. */
+/* GET Home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Index' });
 });
 
-/* GET Applications page. */
-router.get('/packages', function(req, res, next) {
-  res.render('packages', { title: 'Packages' });
+/* GET Destination page. */
+router.get('/destination', function(req, res, next) {
+  res.render('destination', { title: 'Destination' });
 });
 
-/* GET Training Resources page. */
-router.get('/products', function(req, res, next) {
-  res.render('products', { title: 'Products' });
-});
-
-/* GET Codes page. */
-router.get('/gallery', function(req, res, next) {
-  res.render('gallery', { title: 'Gallery' });
+/* GET Book page. */
+router.get('/book', function(req, res, next) {
+  res.render('book', { title: 'Book' });
 });
 
 /* GET About page. */
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About' });
 });
+
+/* GET Login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+
+
 
 module.exports = router;
